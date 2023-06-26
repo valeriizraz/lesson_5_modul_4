ec0d24fcae5560f4c8d318066daa207d8726c9d8
valeriizraz <valeriizraz@gmail.com>
2023-06-26 15:11:24 +0200

    Домашка

--- js/task_02.js
+++ js/task_02.js
@@ -1,2 +1,16 @@
 'use strict'
 
+function isPrime(num) {
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) return false;
+  }
+  return num !== 1;
+}
+
+function printPrimes(max) {
+  for (let i = 2; i <= max; i++) {
+    if (isPrime(i)) console.log(i);
+  }
+}
+
+printPrimes(100);
--- js/task_03.js
+++ js/task_03.js
@@ -1,3 +1,20 @@
 'use strict'
 
 
+// const reverseString = (str) => {
+//   return str.split('').reverse().join('');
+  
+// }
+
+// const result = reverseString('Привет мир!');
+// console.log(result);
+
+function reverseStr(str){
+  let a = "";
+  for (let i = 0; i < str.length; i++){
+    a += str[(str.length - 1) - i];
+  }
+  return a;
+}
+
+console.log(reverseStr('Привет мир'));
